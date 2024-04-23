3365c42ce35e1acf57f16fa46cba3e84a2a6bf6b
a-novi27 <166109235+a-novi27@users.noreply.github.com>
2024-04-23 11:53:04 +0300

    Add files via upload

---
+++ Cicle_For.py
@@ -0,0 +1,9 @@
+list_cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for car in list_cars:
+    print('Я езжу на автомабиле марки', car)
+cars_count = 0
+for cars_count in range(len(list_cars)):
+    cars_count += 10
+    print(cars_count)
+
+
